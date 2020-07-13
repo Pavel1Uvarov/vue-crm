3e40cf7d5cd0c8266de0b1d4b2db003c6bca02dd
Pavel1Uvarov <pavel2uvarov@gmail.com>
2020-07-13 14:30:44 +0300

    Add Project Files

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="app-main-layout">
+      <Navbar @toggleMenu="isOpen = !isOpen" />
+      <sidebar :isOpen="isOpen" />
+      <main class="app-content" :class="{full: !isOpen}">
+        <div class="app-page">
+          <router-view />
+        </div>
+      </main>
+      <div class="fixed-action-btn">
+        <a class="btn-floating btn-large blue" href="#">
+          <i class="large material-icons">add</i>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Navbar from '@/components/app/Navbar';
+import Sidebar from '@/components/app/Sidebar';
+
+export default {
+  name: 'main-layout',
+  data: () => ({
+    isOpen: true
+  }),
+  components: {
+    Navbar, Sidebar
+  },
+}
+</script>
